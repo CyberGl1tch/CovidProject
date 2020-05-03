757a21f0d5d1b2d39ce0cf29b3d9798db2501c74
ishallpass <58013530+ishallpass@users.noreply.github.com>
2020-05-03 18:46:01 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/rand_waypoint_system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rand_waypoint_system.dir/main.cpp.obj"
+  "librand_waypoint_system.dll.a"
+  "rand_waypoint_system.exe"
+  "rand_waypoint_system.exe.manifest"
+  "rand_waypoint_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rand_waypoint_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,24 +1,95 @@
 #include <iostream>
-#include "UserChains.h"
-
+#include <random>
+#include <ctime>
 using namespace std;
-
-int main() {
-    int days;
-    int i;
-    //the seconds every tick counts for 30 seconds do the loop has to be repeated only 2880 times instead of 86400
-    //the time will be multiplied by 30 within the chain
-    int sec;
-    UserChains a;
-    a.add_lick(6,13,13,true);
-    a.display();
-    cout<<"give number of days you whant to rum simulation for : ";
-    cin>>days;
-    for (i=1;i<=days;i++){
-        for (sec=1;sec<=2880;sec++){
-            //where the inner workings will go
-        }
+class random_waypoint_system{
+private:
+    int x;int y;int startpoint_x;int startpoint_y ; int objective_x ; int objective_y;
+public:
+    //initial constructor
+    random_waypoint_system(){
+        //random number for everything
+        startpoint_x =rand()%100;
+        startpoint_y = rand()%100;
+        objective_x = rand()%100;
+        objective_y = rand()%100;
+    }
+    //adds a new objective if you wish
+    void new_objectives(){
+        objective_x=rand()%100;
+        objective_y=rand()%100;
+        cout<<"new objectives"<<endl;
+        cout<<objective_x<<"      "<<objective_y<<endl;
     }
 
+    //getters
+    int get_startpoint_x(){
+        return startpoint_x;
+    }
+    int get_startpoint_y(){
+        return startpoint_y;
+    }
+    int get_objective_x(){
+        return objective_x;
+    }
+
+    int get_objective_y(){
+        return objective_y;
+    }
+    //to perpatima oso dougrou paei
+    void walk() {
+        int distance_y = startpoint_y - objective_y;
+        distance_y = abs(distance_y);
+        int distance_x = startpoint_x - objective_x;
+        distance_x = abs(distance_x);
+        if (startpoint_x < objective_x) {
+            startpoint_x = startpoint_x + (rand() % 3) + 2;
+        } else if (startpoint_x > objective_x) {
+            startpoint_x = abs(startpoint_x - ((rand() % 3) + 2));
+        } else if (distance_x < 5) {//an einai kato apo 5 apostas tote dn kathom na to kano random and shit o bazw amesos
+            if (startpoint_x < objective_x) {
+                startpoint_x = startpoint_x + distance_x;
+            } else if (startpoint_x > objective_x) {
+                startpoint_x = abs(startpoint_x - distance_x);
+            }
+
+        }
+        if (startpoint_y < objective_y) {
+            startpoint_y = startpoint_y + ((rand() % 3) + 2);
+
+        } else if (startpoint_y > objective_y) {
+            startpoint_y = abs(startpoint_y - ((rand() % 3) + 2));
+        } else if (distance_y < 5) {//same me to apo pano
+            if (startpoint_y < objective_y) {
+                startpoint_y = startpoint_y + distance_y;
+            } else if (startpoint_y > objective_y) {
+                startpoint_y = abs(startpoint_y - objective_y);
+
+            }
+        }
+        cout << startpoint_x << "  " << startpoint_y << endl;
+    }
+
+    //just a testing function nothing more
+    //shows the original values
+    void print(){
+        cout<<startpoint_x<<"    "<<startpoint_y<<endl;
+        cout<<objective_x<<"    "<<objective_y<<endl;
+    }
+};
+int main() {
+    srand(time(0));//very important
+    random_waypoint_system a;
+    //print before walking
+    a.print();
+    //walks until everything is where it should be
+    for (int i;i<100;i++){
+        a.walk();
+        if(a.get_startpoint_y()==a.get_objective_y()&&a.get_startpoint_x()==a.get_objective_x()){
+            a.new_objectives();
+        }
+    }
+    //in this print the 2 numbers should be the same
+    a.print();
     return 0;
 }
